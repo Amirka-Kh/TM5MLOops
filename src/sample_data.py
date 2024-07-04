0f613d23951b69fbe605ed7d13982982c3c03de5
Amirka-Kh <amirabikhuja@gmail.com>
2024-07-04 17:08:14 +0300

    Fix errors

--- src/sample_data.py
+++ src/sample_data.py
@@ -2,10 +2,13 @@ import pandas as pd
 import hydra
 from omegaconf import DictConfig, OmegaConf
 import os
+import datetime
 
 
-@hydra.main(version_base=None, config_path="../configs", config_name="main")
+@hydra.main(version_base="1.2", config_path="../configs", config_name="main")
 def sample_data(cfg: DictConfig) -> None:
+    start = datetime.datetime.now()
+
     # Read data from URL
     data = pd.read_csv(cfg.data.url)
 
@@ -17,17 +20,20 @@ def sample_data(cfg: DictConfig) -> None:
     sample = data.sample(frac=cfg.data.sample_size, random_state=int(minor))
 
     # Create the output directory if it doesn't exist
-    os.makedirs("data/samples", exist_ok=True)
+    os.makedirs("../data/samples", exist_ok=True)
 
     # Save the sample data to CSV
-    sample.to_csv(f"data/samples/{cfg.data.dataset_name}", index=False)
+    sample.to_csv(f"../data/samples/{cfg.data.dataset_name}", index=False)
 
     # Update the version in the Hydra configuration
     new_version = increment_version(version)
-    cfg.data.version = new_version
-    cfg.data.message = f"Add data version {new_version}"
-    with open(cfg.config_path, 'w') as config_file:
-        OmegaConf.save(config=cfg, f=config_file)
+    new_message = f"Add data version {new_version}"
+    OmegaConf.update(cfg, 'data.version', new_version)
+    OmegaConf.update(cfg, 'data.message', new_message)
+
+    end = datetime.datetime.now()
+    print(f"Data sampling was successful")
+    print('time: ', end-start)
 
 
 def increment_version(version: str) -> str:
--- src/version_data.py
+++ src/version_data.py
@@ -1,27 +1,27 @@
 import subprocess
 import hydra
 from omegaconf import DictConfig
+import os
 
 
-@hydra.main(config_path="../configs", config_name="main")
+@hydra.main(version_base="1.2", config_path="../configs", config_name="main")
 def version_data(cfg: DictConfig):
     version = cfg.data.version
     message = cfg.data.message
 
     # Stage and commit changes using DVC
-    subprocess.run(["dvc", "add", "../data/samples/sample.csv"], check=True)
-    subprocess.run(["git", "add", "."], check=True)
-    subprocess.run(["git", "commit", "-m", {message}], check=True)
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "-m", message])
 
     # Tag the commit with the version number
-    subprocess.run(["git", "tag", "-a", version, "-m", message], check=True)
+    subprocess.run(["git", "tag", "-a", version, "-m", message])
 
     # Push the commit and the tag to the remote repository
-    subprocess.run(["git", "push"], check=True)
-    subprocess.run(["git", "push", "--tags"], check=True)
+    subprocess.run(["git", "push"])
+    subprocess.run(["git", "push", "--tags"])
 
     # Push the data using DVC
-    subprocess.run(["dvc", "push"], check=True)
+    subprocess.run(["dvc", "push"])
 
     print(f"Data versioned successfully with version {version}.")
 
