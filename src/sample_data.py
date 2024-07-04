148b520849a9f5958bd237cd1a6e2e4f8cd52794
Amirka-Kh <amirabikhuja@gmail.com>
2024-07-04 15:29:06 +0300

    Update sampling function

--- src/sample_data.py
+++ src/sample_data.py
@@ -2,17 +2,19 @@ import pandas as pd
 import hydra
 from omegaconf import DictConfig, OmegaConf
 import os
-import datetime
+
 
 @hydra.main(version_base=None, config_path="../configs", config_name="main")
 def sample_data(cfg: DictConfig) -> None:
-    print(OmegaConf.to_yaml(cfg))
-    start = datetime.datetime.now()
     # Read data from URL
     data = pd.read_csv(cfg.data.url)
 
+    # Take seed for random sampling
+    version = cfg.data.version
+    major, minor = map(int, version.split('.'))
+
     # Sample the data
-    sample = data.sample(frac=cfg.data.sample_size)
+    sample = data.sample(frac=cfg.data.sample_size, random_state=int(minor))
 
     # Create the output directory if it doesn't exist
     os.makedirs("data/samples", exist_ok=True)
@@ -20,9 +22,19 @@ def sample_data(cfg: DictConfig) -> None:
     # Save the sample data to CSV
     sample.to_csv(f"data/samples/{cfg.data.dataset_name}", index=False)
 
-    end = datetime.datetime.now()
+    # Update the version in the Hydra configuration
+    new_version = increment_version(version)
+    cfg.data.version = new_version
+    cfg.data.message = f"Add data version {new_version}"
+    with open(cfg.config_path, 'w') as config_file:
+        OmegaConf.save(config=cfg, f=config_file)
 
-    print(end - start)
+
+def increment_version(version: str) -> str:
+    major, minor = map(int, version.split('.'))
+    minor += 1
+    return f"{major}.{minor}"
+
 
 if __name__ == "__main__":
     sample_data()
--- src/version_data.py
+++ src/version_data.py
@@ -5,13 +5,13 @@ from omegaconf import DictConfig
 
 @hydra.main(config_path="../configs", config_name="main")
 def version_data(cfg: DictConfig):
-    version = cfg.version
-    message = cfg.message if "message" in cfg else "Versioning data"
+    version = cfg.data.version
+    message = cfg.data.message
 
     # Stage and commit changes using DVC
     subprocess.run(["dvc", "add", "../data/samples/sample.csv"], check=True)
     subprocess.run(["git", "add", "."], check=True)
-    subprocess.run(["git", "commit", "-m", f"Add version {version} of the data"], check=True)
+    subprocess.run(["git", "commit", "-m", {message}], check=True)
 
     # Tag the commit with the version number
     subprocess.run(["git", "tag", "-a", version, "-m", message], check=True)
