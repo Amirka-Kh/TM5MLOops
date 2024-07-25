dda88dc7f6f436e24c140f62d0206572af07b4be
Amirka-Kh <amirabikhuja@gmail.co>
2024-07-25 22:23:02 +0300

    Add data version 1.27

--- src/validate.py
+++ src/validate.py
@@ -1,4 +1,5 @@
 from model import load_features
+from sample_data import read_datastore
 from transform_data import transform_data # custom module
 from model import retrieve_model_with_alias # custom module
 from utils import init_hydra # custom module
@@ -9,17 +10,14 @@ import mlflow
 
 cfg = init_hydra()
 
-version  = cfg.test_data_version
+df, version = read_datastore()
 
-X, y = load_features(name="features_target", version=version)
+TARGET_COLUMN = df['log_price'][0]
 
-# Specify categorical columns and target column
-TARGET_COLUMN = cfg.data.target_cols[0]
+CATEGORICAL_COLUMNS = df.select_dtypes(include=['object']).columns
 
-categorical_features = X_train.select_dtypes(include=['object']).columns
-
-dataset_name = cfg.data.dataset_name
-
+# dataset_name = cfg.data.dataset_name
+dataset_name = 'validation_dataset'
 
 # Wrap your Pandas DataFrame with giskard.Dataset (validation or test set)
 giskard_dataset = giskard.Dataset(
@@ -28,3 +26,34 @@ giskard_dataset = giskard.Dataset(
     name=dataset_name, # Optional: Give a name to your dataset
     cat_columns=CATEGORICAL_COLUMNS  # List of categorical columns. Optional, but improves quality of results if available.
 )
+
+model_name = cfg.model.best_model_name
+
+# You can sweep over challenger aliases using Hydra
+model_alias = cfg.model.best_model_alias
+
+model: mlflow.pyfunc.PyFuncModel = retrieve_model_with_alias(model_name, model_alias = model_alias)
+
+client = mlflow.MlflowClient()
+
+mv = client.get_model_version_by_alias(name = model_name, alias=model_alias)
+
+model_version = mv.version
+
+transformer_version = cfg.data_transformer_version
+
+def predict(raw_df):
+    X = transform_data(
+        df = raw_df,
+        version = version,
+        cfg = cfg,
+        return_df = False,
+        only_transform = True,
+        transformer_version = transformer_version,
+        only_X = True
+      )
+
+    return model.predict(X)
+
+predictions = predict(df[df.columns].head())
+print(predictions)
